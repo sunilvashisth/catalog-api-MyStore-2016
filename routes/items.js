c006aaefb8629138e612514d8cf84621d947192d
Sunil Vashisth <suvashis@in.ibm.com>
2016-09-27 05:24:04 -0500

    fixes

--- routes/items.js
+++ routes/items.js
@@ -10,8 +10,8 @@ var USE_FASTCACHE = false;
  * 2. uncomment the remaining method body
  */
 exports.loadTest = function(req, res) {
-    res.json({"success": 0, "fail": 0, "time": 0});
-/*
+//    res.json({"success": 0, "fail": 0, "time": 0});
+///*
     USE_FASTCACHE = true;
     var testCount = req.query.count;
     testCount = testCount ? parseInt(testCount) : 100;
