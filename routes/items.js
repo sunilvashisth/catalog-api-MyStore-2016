74eceeef19af7ca4780aa7b56cbc5cc754f72e2f
Sunil Vashisth <suvashis@in.ibm.com>
2016-09-29 06:56:39 -0500

    Fixes sunilvashisth/ui-MyStore-2016#2

--- routes/items.js
+++ routes/items.js
@@ -11,7 +11,7 @@ var USE_FASTCACHE = false;
  */
 exports.loadTest = function(req, res) {
     res.json({"success": 0, "fail": 0, "time": 0});
-/*
+
     USE_FASTCACHE = true;
     var testCount = req.query.count;
     testCount = testCount ? parseInt(testCount) : 100;
@@ -40,7 +40,7 @@ exports.loadTest = function(req, res) {
             path: "/items/" + (i % 2 ? itemId1 : itemId2)
 	}, callback);
     }
-*/
+
 };
 
 
